c9a50d313825f1a47c16678eef01fb52bfbd15ab
YdrMaster <ydrml@hotmail.com>
2019-04-17 12:50:40 +0800

    界面

--- PM1.SDK.Net/PM1.TestTool/MainWindowItems/DriveTab/DriveTab.xaml.cs
+++ PM1.SDK.Net/PM1.TestTool/MainWindowItems/DriveTab/DriveTab.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
 
 namespace Autolabor.PM1.TestTool.MainWindowItems.DriveTab {
     /// <summary>
     /// DriveTab.xaml 的交互逻辑
     /// </summary>
     public partial class DriveTab : UserControl, ITabControl {
+        private MainWindowContext _windowContext;
         private TabContext _tabContext;
 
         public DriveTab() => InitializeComponent();
@@ -13,7 +17,24 @@ namespace Autolabor.PM1.TestTool.MainWindowItems.DriveTab {
 
         public void OnLeave() { }
 
-        private void Grid_DataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        private void Tab_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+            => _windowContext = e.NewValue as MainWindowContext;
+
+        private void Grid_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
             => _tabContext = e.NewValue as TabContext;
+
+        private void Drag_MouseMove(object sender, MouseEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                if (e.MouseDevice.Captured == null)
+                    e.MouseDevice.Capture(Origin);
+                var position = e.GetPosition(Origin);
+                _tabContext.X = position.X;
+                _tabContext.Y = position.Y;
+            } else {
+                e.MouseDevice.Capture(null);
+                _tabContext.X =
+                _tabContext.Y = TabContext.Size / 2;
+            }
+        }
     }
 }
--- PM1.SDK.Net/PM1.TestTool/MainWindowItems/DriveTab/DriveTabContext.cs
+++ PM1.SDK.Net/PM1.TestTool/MainWindowItems/DriveTab/DriveTabContext.cs
@@ -1,28 +1,66 @@
 ﻿using System;
+using System.Windows;
 
 namespace Autolabor.PM1.TestTool.MainWindowItems.DriveTab {
     internal class TabContext : BindableBase {
 
         public static double Size => 360;
-
-        public static double TouchSize => 30;
-
+        public static double TouchSize => 40;
+        public static double Radius => (Size - TouchSize) / 2;
         public static double MaxSpeed => 6 * Math.PI;
 
-        private double _speed;
+        private double _speed,
+                       _x,
+                       _y;
 
-        public TabContext() 
-            => _speed = 0.1 * MaxSpeed;
+        public TabContext() {
+            _speed = 0.1 * MaxSpeed;
+            _x = _y = Size / 2;
+        }
 
         public double Speed {
             get => _speed;
             set {
-                if(!SetProperty(ref _speed, value))return;
+                var pre = _speed;
+                if (!SetProperty(ref _speed, value)) return;
                 Notify(nameof(SpeedRatioText));
+
+                if (pre <= 0) return;
+                _x *= _speed / pre;
+                _y *= _speed / pre;
             }
         }
 
         public string SpeedRatioText
             => ToolFunctions.Format("0.%", _speed / MaxSpeed);
+
+        public double X {
+            get => _x;
+            set {
+                if (!SetProperty(ref _x, value)) return;
+                Notify(nameof(LimitedLeft));
+            }
+        }
+
+        public double Y {
+            get => _y;
+            set {
+                if (!SetProperty(ref _y, value)) return;
+                Notify(nameof(LimitedTop));
+            }
+        }
+
+        public double LimitedLeft => Limit(_x, _y).x;
+
+        public double LimitedTop => Limit(_x, _y).y;
+
+        private (double x, double y) Limit(double x, double y) {
+            var xo = x - Size / 2;
+            var yo = y - Size / 2;
+            var tan = Math.Atan2(yo, xo);
+            var r = Math.Min(Radius, Math.Sqrt(xo * xo + yo * yo));
+            return (r * Math.Cos(tan) + Radius,
+                    r * Math.Sin(tan) + Radius);
+        }
     }
 }
