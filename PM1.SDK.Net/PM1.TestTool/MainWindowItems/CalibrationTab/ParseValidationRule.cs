f59787abfb6a8cf60add4b2e2ba74bc32210369f
YdrMaster <ydrml@hotmail.com>
2019-04-23 13:01:40 +0800

    解决一些问题

---
+++ PM1.SDK.Net/PM1.TestTool/MainWindowItems/CalibrationTab/ParseValidationRule.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Autolabor.PM1.TestTool.MainWindowItems.CalibrationTab {
+    /// <summary>
+    ///     范围检查
+    /// </summary>
+    internal class ParseValidationRule : ValidationRule {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+            => double.TryParse(value as string, out var _value)
+               ? ValidationResult.ValidResult
+               : new ValidationResult(false, "无法解析");
+    }
+}
---
+++ PM1.SDK.Net/PM1.TestTool/MainWindowItems/CalibrationTab/ValueFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Autolabor.PM1.TestTool.MainWindowItems.CalibrationTab {
+    [ValueConversion(typeof(double), typeof(string))]
+    internal class ValueFormatter : IValueConverter {
+        public string Format { get; set; } = "0.###";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => ((double)value).ToString(Format, culture);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
