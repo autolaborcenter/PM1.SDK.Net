a7d3a2f442d7777890e5f642998743cd8b3eaa59
YdrMaster <ydrml@hotmail.com>
2019-04-30 10:16:47 +0800

    邪魔退散

--- PM1.SDK.Net/PM1.TestTool/MainWindowItems/ActionTab/ActionView.xaml.cs
+++ PM1.SDK.Net/PM1.TestTool/MainWindowItems/ActionTab/ActionView.xaml.cs
@@ -4,7 +4,6 @@ using System.Globalization;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace Autolabor.PM1.TestTool.MainWindowItems.ActionTab {
     /// <summary>
